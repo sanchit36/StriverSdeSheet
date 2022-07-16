ee0a1e0b3ef7a1214e851c290f4df2b1c5a0e3f9
Sanchit Bhadgal <sanchitbhadgal36@gmail.com>
2022-07-16 12:12:51 +0530

    Added Lis sol

--- Day 25] Dynamic Programming/02_Longest_Increasing_Subsequence.cpp
+++ Day 25] Dynamic Programming/02_Longest_Increasing_Subsequence.cpp
@@ -41,3 +41,65 @@ int lengthOfLIS(vector<int> &nums)
     }
     return dp[0][0];
 }
+
+// Tabulation with backtracking the LIS
+// TC: O(N^2) SC: O(N)
+int longestIncreasingSubsequence(int arr[], int n)
+{
+
+    int maxi = 1;
+    vector<int> dp(n, 1), hash(n);
+    int lastIndex = 0;
+    for (int ind = 0; ind < n; ind++)
+    {
+        hash[ind] = ind;
+        for (int prev = 0; prev < ind; prev++)
+        {
+            if (arr[prev] < arr[ind] && 1 + dp[prev] > dp[ind])
+            {
+                dp[ind] = 1 + dp[prev];
+                hash[ind] = prev;
+            }
+        }
+        if (dp[ind] > maxi)
+        {
+            maxi = dp[ind];
+            lastIndex = ind;
+        }
+    }
+    vector<int> lis;
+    lis.push_back(arr[lastIndex]);
+    while (hash[lastIndex] != lastIndex)
+    {
+        lastIndex = hash[lastIndex];
+        lis.push_back(arr[lastIndex]);
+    }
+    reverse(lis.begin(), lis.end());
+    for (int num : lis)
+        cout << num << " ";
+    cout << endl;
+    return maxi;
+}
+
+// LIS USING Binary Search
+// TC: Nlog(N)
+int longestIncreasingSubsequence(int arr[], int n)
+{
+    vector<int> temp;
+    temp.push_back(arr[0]);
+    int len = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (temp.back() < arr[i])
+        {
+            temp.push_back(arr[i]);
+            len++;
+        }
+        else
+        {
+            int prev = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+            temp[prev] = arr[i];
+        }
+    }
+    return len;
+}
